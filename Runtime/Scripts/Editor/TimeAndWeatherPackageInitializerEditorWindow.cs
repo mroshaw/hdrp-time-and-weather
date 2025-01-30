91a5c91551e8084d0d77c803d4498237355878ce
Oli Ollerenshaw <oli@mroshaw.co.uk>
2025-01-30 15:22:32 +0000

    Hide samples folder from main package

---
+++ Runtime/Scripts/Editor/TimeAndWeatherPackageInitializerEditorWindow.cs
@@ -0,0 +1,20 @@
+using DaftAppleGames.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace DaftAppleGames.TimeAndWeather.Editor
+{
+    public class TimeAndWeatherPackageInitializerEditorWindow : PackageInitializerEditorWindow
+    {
+        [MenuItem("Daft Apple Games/Packages/HDRP Time And Weather Initializer")]
+        public static void ShowWindow()
+        {
+            TimeAndWeatherPackageInitializerEditorWindow packageInitWindow = GetWindow<TimeAndWeatherPackageInitializerEditorWindow>();
+            packageInitWindow.titleContent = new GUIContent("HDRP Time and Weather");
+        }
+        protected override string GetIntroText()
+        {
+            return "Welcome to HDRP Time And Weather!";
+        }
+    }
+}
